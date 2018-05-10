9e52223ee840630b43cdb69d66e75af74b0cea71
TheAlaskanBullworm <39092543+TheAlaskanBullworm@users.noreply.github.com>
2018-05-10 09:45:03 -0400

    Update Main.js

--- Main.js
+++ Main.js
@@ -18,11 +18,11 @@
   @-webkit-keyframes side-slide {
     0% {
       opacity: 0;
-      -webkit-transform: translateY(70%);
+      -webkit-transform: translateX(70%);
     } 
     100% {
       opacity: 1;
-      -webkit-transform: translateY(0%);
+      -webkit-transform: translateX(0%);
     }
   }
   @media screen and (max-width: 768px) {
